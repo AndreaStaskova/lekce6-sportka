74916212fc7014c2ab1a28982207b33e3a05a9a5
Andrea Staskova <andrea@intiveo.com>
2021-04-15 15:07:46 +0200

    [FEATURE] main functionality added

--- main.js
+++ main.js
@@ -6,3 +6,66 @@
 // Z každého vylosovaného čísla vygeneruj následující HTML kód:
 // <span class="cislo">8</span>
 // ... který pak přidej dovnitř prvku <div id="vyherni-cisla">:
+
+let osudi = [];
+let los;
+let tah = [];
+let historieTahu = [];
+let vyherniCisla = document.getElementById("vyherni-cisla");
+
+function naplnOsudi () {
+    for (let i = 1; i <= 48; i++) {
+        osudi.push(i);
+    }
+}
+
+naplnOsudi()
+
+let timeOut
+
+/*
+
+function delayedNumber() {
+    timeOut = window.setTimeout(losuj(), 2000)
+}
+
+*/
+
+function losuj() {
+    vyherniCisla.innerHTML = " "
+    
+    for (let i = 0; i < 7; i++) {
+        let vyherniIndex = Math.floor(Math.random() * osudi.length);
+        let vyherniCislo = osudi[vyherniIndex];
+    
+        tah.push(vyherniCislo);
+        osudi.splice(vyherniIndex, 1);
+    
+        console.log(osudi);
+        console.log(tah);
+    }
+    vypisTah();
+    osudi = [];
+    
+    vypisHistorii();
+    tah = [];
+    naplnOsudi();
+}
+
+function vypisTah() {    
+
+    for (let i = 0; i < tah.length; i++) {
+        vyherniCisla.innerHTML += "<span class='cislo'>" + tah[i] + "</span>";
+    }
+}
+
+function vypisHistorii() {
+    historieTahu.push(tah);
+
+    let predesleTahy = document.getElementById("historie-tahu");
+    predesleTahy.textContent = historieTahu
+    
+}
+   
+
+
